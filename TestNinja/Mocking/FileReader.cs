5218af66d1242ea403c252fa3fadb3f795d92787
HubTicaBB <tijana.ilic87@protonmail.com>
2020-11-30 00:50:44 +0100

    Extract IFileReader from FileReader class

--- TestNinja/Mocking/FileReader.cs
+++ TestNinja/Mocking/FileReader.cs
@@ -2,11 +2,16 @@
 
 namespace TestNinja.Mocking
 {
-    public class FileReader
+    public interface IFileReader
+    {
+        string Read(string path);
+    }
+
+    public class FileReader : IFileReader
     {
         public string Read(string path)
         {
-            return File.ReadAllText("video.txt");
+            return File.ReadAllText(path);
         }
     }
 }
