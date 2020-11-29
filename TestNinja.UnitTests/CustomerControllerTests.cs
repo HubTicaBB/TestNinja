99293fd7600e182c9bead4836d73db0689a09a62
HubTicaBB <tijana.ilic87@protonmail.com>
2020-11-29 19:00:28 +0100

    Add SetUp and create GetCustomer_IdIsNotZero_ReturnOk

--- TestNinja.UnitTests/CustomerControllerTests.cs
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -9,18 +9,29 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class CustomerControllerTests
     {
+        private CustomerController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new CustomerController();
+        }
+
         [Test]
         public void GetCustomer_IdIsZero_ReturnNotFound() 
         {
-            var controller = new CustomerController();
-
-            var result = controller.GetCustomer(0);
+            var result = _controller.GetCustomer(0);
 
             Assert.That(result, Is.TypeOf<NotFound>());
             Assert.That(result, Is.InstanceOf<NotFound>()); // Covers derivatives as well
         }
 
         [Test]
-        public void GetCustomer_IdIsNotZero_ReturnOk() {  }
+        public void GetCustomer_IdIsNotZero_ReturnOk() 
+        {
+            var result = _controller.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
     }
 }
