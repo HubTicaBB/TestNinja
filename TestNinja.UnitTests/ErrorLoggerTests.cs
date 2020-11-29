cf4a03280f241e72571bd0cc9f2085bf239f6ebb
HubTicaBB <tijana.ilic87@protonmail.com>
2020-11-29 19:06:48 +0100

    Testng Void Methods

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("abc");
+
+            Assert.That(logger.LastError, Is.EqualTo("abc"));
+        }
+    }
+}
