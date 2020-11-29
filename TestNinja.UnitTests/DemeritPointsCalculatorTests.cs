985b364803579fcb490658bd133ba639fc4ee9a4
HubTicaBB <tijana.ilic87@protonmail.com>
2020-11-29 21:04:15 +0100

    Cfreate DemeritPointsCalculatorTests

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _demeritPointsCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _demeritPointsCalculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException(int speed)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _demeritPointsCalculator.CalculateDemeritPoints(speed));
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        public void CalculateDemeritPoints_SpeedIsLessThanOrEqualAsSpeedLimit_ReturnZero(int speed, int expectedResult)
+        {
+            var result = _demeritPointsCalculator.CalculateDemeritPoints(speed);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_SpeedHigherThanSpeedLimit_ReturnDemeritPoints()
+        {
+            int result = _demeritPointsCalculator.CalculateDemeritPoints(70);
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
