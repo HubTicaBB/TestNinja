eb8978babe781c3c08151114c779645c83d5852c
HubTicaBB <tijana.ilic87@protonmail.com>
2020-11-30 00:45:26 +0100

    Extract the code talking to an external resource to a separate class

---
+++ TestNinja/Mocking/FileReader.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace TestNinja.Mocking
+{
+    public class FileReader
+    {
+        public string Read(string path)
+        {
+            return File.ReadAllText("video.txt");
+        }
+    }
+}
