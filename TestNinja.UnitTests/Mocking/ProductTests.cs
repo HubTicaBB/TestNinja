eb41de98c5ecfa9ae2b1e070fc0f2577da158089
HubTicaBB <tijana.ilic87@protonmail.com>
2020-11-30 21:31:41 +0100

    Create ProductTests
    (using 2 classes, but no mocks)

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_ReturnReducedPrice()
+        {
+            var product = new Product() { ListPrice = 100 };
+            var customer = new Customer() { IsGold = true };
+
+            var result = product.GetPrice(customer);
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+
+        [Test]
+        public void GetPrice_NonGoldCustomer_ReturnFullPrice()
+        {
+            var product = new Product() { ListPrice = 100 };
+            var customer = new Customer() { IsGold = false };
+
+            var result = product.GetPrice(customer);
+
+            Assert.That(result, Is.EqualTo(100));
+        }
+    }
+}
