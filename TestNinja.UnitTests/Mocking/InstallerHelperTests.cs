d36076614cf85ca358e0a7440a5ba8419d13b2e6
HubTicaBB <tijana.ilic87@protonmail.com>
2020-12-01 00:46:11 +0100

    Create InstallerHelperTests
    NOTE!
    When programming a Mock, you need to pass the
    exactly same argument as in production code,
    "http://example.com/customerName/installerName" in this example

---
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Net;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _fileDownloader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            _fileDownloader.Setup(fd => fd.DownloadFile("http://example.com/customerName/installerName", null)).Throws<WebException>();
+            var result = _installerHelper.DownloadInstaller("customerName", "installerName");
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadSuccessful_ReturnTrue()
+        {
+            var result = _installerHelper.DownloadInstaller("customerName", "installerName");
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
