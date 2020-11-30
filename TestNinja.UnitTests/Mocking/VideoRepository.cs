0f2fb745715d30f006905bb4cb4f6dd3f68cac17
HubTicaBB <tijana.ilic87@protonmail.com>
2020-11-30 22:53:44 +0100

    Refactoring GetUnprocessedVideosAsCsv method

---
+++ TestNinja.UnitTests/Mocking/VideoRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TestNinja.Mocking;
+using System.Linq;
+
+
+namespace TestNinja.UnitTests.Mocking
+{
+    public class VideoRepository
+    {
+        public IEnumerable<Video> GetUnprocessedVideos()
+        {
+            using var context = new VideoContext();
+            var videos = (from video in context.Videos
+                          where !video.IsProcessed
+                          select video).ToList();
+
+            return videos;
+        }
+    }
+}
